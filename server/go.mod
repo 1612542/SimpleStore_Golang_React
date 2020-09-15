8a9b1573b4c3f095b60f615aacac354c5640b293
Tran Ngoc Quoc <quoc.tn@teko.vn>
2020-09-15 12:16:59 +0700

    connect postgres sql

--- server/go.mod
+++ server/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.1.1
 	github.com/spf13/viper v1.7.1
 )
--- server/main.go
+++ server/main.go
@@ -1,50 +1,51 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	// "encoding/json"
+	// "fmt"
 	"log"
-	"server/config"
 	"server/connector"
 	"server/models"
 	"server/repository"
 	"server/service"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	// Open up db connection
-	db := connector.ConnectDB(config.MySQLConfig())
+	// db := connector.ConnectDB(config.MySQLConfig())
+	db := connector.ConnectDB()
 
-	productRepo := repository.NewMysqlProductRepository(db)
-	productService := service.NewProductService(productRepo)
-	products, err := productService.GetProducts()
-	if err != nil {
-		log.Fatal(err)
-	}
-	j, err := json.Marshal(products)
-	fmt.Println(string(j))
+	// productRepo := repository.NewMysqlProductRepository(db)
+	// productService := service.NewProductService(productRepo)
+	// products, err := productService.GetProducts()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// j, err := json.Marshal(products)
+	// fmt.Println(string(j))
 
 	employeeRepo := repository.NewMysqlEmployeeRepository(db)
 	employeeService := service.NewEmployeeService(employeeRepo)
-	employee1 := models.Employee{EmployeeId: "E2", EmployeeName: "employee2"}
-	_, err1 := employeeService.CreateEmployee(employee1)
-	if err1 != nil {
-		log.Fatal(err1)
+	employee1 := models.Employee{EmployeeId: "E1", EmployeeName: "employee1"}
+	_, err := employeeService.CreateEmployee(employee1)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	employees, err := employeeService.GetEmployees()
-	k, err := json.Marshal(employees)
-	fmt.Println(string(k))
+	// employees, err := employeeService.GetEmployees()
+	// k, err := json.Marshal(employees)
+	// fmt.Println(string(k))
 
-	employee2 := models.Employee{EmployeeId: "E3", EmployeeName: "employee3"}
-	_, err2 := employeeService.CreateEmployee(employee2)
-	if err2 != nil {
-		log.Fatal(err2)
-	}
+	// employee2 := models.Employee{EmployeeId: "E3", EmployeeName: "employee3"}
+	// _, err2 := employeeService.CreateEmployee(employee2)
+	// if err2 != nil {
+	// 	log.Fatal(err2)
+	// }
 
-	employees1, err := employeeService.GetEmployees()
-	k1, err := json.Marshal(employees1)
-	fmt.Println(string(k1))
+	// employees1, err := employeeService.GetEmployees()
+	// k1, err := json.Marshal(employees1)
+	// fmt.Println(string(k1))
 }
